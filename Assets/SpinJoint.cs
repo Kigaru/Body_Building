4bd74514015d374cbf6072ac46327790d5906926
ciaranmalone <ciaranmalone27@gmail.com>
2020-01-31 23:37:12 +0000

    arms kinda working

--- Assets/SpinJoint.cs
+++ Assets/SpinJoint.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class SpinJoint : MonoBehaviour
 {
     HingeJoint2D hinge;
+    JointMotor2D motor;
+    KeyCode key;
 
     void Start()
     {
         hinge = GetComponent<HingeJoint2D>();
-
+        hinge.useMotor = true;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        hinge.useMotor = true;
+
+        motor = hinge.motor;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            motor.motorSpeed = 400;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            motor.motorSpeed = -400;
+        }
+        else
+        {
+            motor.motorSpeed = 0;
+        }
+        hinge.motor = motor;
+
     }
 }
