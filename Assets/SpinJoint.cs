a7be3233221e6a67c5f61522bd34e9e9aeab8d92
ciaranmalone <ciaranmalone27@gmail.com>
2020-01-31 23:12:40 +0000

    groovy

---
+++ Assets/SpinJoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinJoint : MonoBehaviour
+{
+    HingeJoint2D hinge;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hinge.useMotor = true;
+    }
+}
--- Assets/_Scripts/limbScript.cs
+++ Assets/_Scripts/limbScript.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class limbScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb2D;
     void Start()
     {
-        
+
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.rotation = 45f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb2D.rotation += 1.0f;
     }
 }
