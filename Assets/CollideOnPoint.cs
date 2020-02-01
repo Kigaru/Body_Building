f64edbd5eac9d709c2703ac9de3365c71fc77c4e
ciaranmalone <ciaranmalone27@gmail.com>
2020-02-01 17:14:23 +0000

    Arm swiggle2,0

--- Assets/CollideOnPoint.cs
+++ Assets/CollideOnPoint.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollideOnPoint : MonoBehaviour
 {
     HingeJoint2D hinge;
+    TextMeshPro textmeshPro;
+
     bool IsConnected = false;
+    KeyCode button;
+    string leftButtons = "QAZWSXEDCRFVT";
+    string rightButtons = "GBYHNUJMIKOLP";
     // Start is called before the first frame update
     void Start()
     {
         hinge = GetComponent<HingeJoint2D>();
         hinge.enabled = false;
         GetComponent<SpinJoint>().enabled = false;
-
+        textmeshPro = GetComponentInChildren<TextMeshPro>();
     }
-
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        int randomInt = Random.Range(0, 12);
+        print(randomInt);
         if (IsConnected == false && col.gameObject.tag == "Limb")
         {
             hinge.enabled = true;
@@ -57,13 +64,19 @@ public class CollideOnPoint : MonoBehaviour
             transform.parent = col.transform.parent;
             if(col.GetComponent<SpinJoint>().isPlayerTwo())
             {
-                GetComponent<SpinJoint>().setButton(KeyCode.P);
+                button = (KeyCode)System.Enum.Parse(typeof(KeyCode), rightButtons[randomInt].ToString());
+                GetComponent<SpinJoint>().setButton(button);
                 GetComponent<SpinJoint>().setPlayerTwo(true);
+                textmeshPro.SetText(rightButtons[randomInt].ToString());
+
             }
             else
             {
-                GetComponent<SpinJoint>().setButton(KeyCode.R);
+                button = (KeyCode)System.Enum.Parse(typeof(KeyCode), leftButtons[randomInt].ToString());
+                GetComponent<SpinJoint>().setButton(button);
                 GetComponent<SpinJoint>().setPlayerTwo(false);
+                textmeshPro.SetText(leftButtons[randomInt].ToString());
+
 
             }
             //Player 1:
