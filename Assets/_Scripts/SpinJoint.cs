3f3cc001974b0d5736a117d757739ed791713431
Kigaru <jakubster9@gmail.com>
2020-02-01 02:19:35 +0000

    Cleaned up the code a little bit, and fixed the direction in which the arms go

--- Assets/_Scripts/SpinJoint.cs
+++ Assets/_Scripts/SpinJoint.cs
@@ -6,78 +6,48 @@ public class SpinJoint : MonoBehaviour
 {
     HingeJoint2D hinge;
     JointMotor2D motor;
-    KeyCode key;
     [SerializeField]
-    private KeyCode p1button;
-    [SerializeField]
-    private KeyCode p1button2;
-    [SerializeField]
-    private KeyCode p2button;
-    [SerializeField]
-    private KeyCode p2button2;
+    private KeyCode button;
 
-    public bool p2;
-
+    [SerializeField]
+    private bool player2;
 
     void Start()
     {
         hinge = GetComponent<HingeJoint2D>();
         hinge.useMotor = true;
-      
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        motor = hinge.motor;
-
-        if (p2 == true)
-        {
-
-            if (Input.GetKey(p2button2))
+       motor = hinge.motor;
+       if (Input.GetKey(button))
+       {
+            if (player2 == true)
+            {
+                motor.motorSpeed = 800;
+                hinge.useMotor = true;
+            }
+            else
+            {
+                motor.motorSpeed = -800;
+                hinge.useMotor = true;
+            }
+       }
+       else
+       {
+            if (player2 == true)
+            {
+                motor.motorSpeed = -400;
+                hinge.useMotor = true;
+            }
+            else
             {
                 motor.motorSpeed = 400;
                 hinge.useMotor = true;
 
             }
-            else if (Input.GetKey(p2button))
-            {
-                motor.motorSpeed = -400;
-                hinge.useMotor = true;
-
-            }
-            else
-            {
-                motor.motorSpeed = 0;
-                hinge.useMotor = false;
-
-            }
+       }
             hinge.motor = motor;
-        }
-
-        else {
-
-            if (Input.GetKey(p1button))
-            {
-                motor.motorSpeed = 400;
-                hinge.useMotor = true;
-
-            }
-            else if (Input.GetKey(p1button2))
-            {
-                motor.motorSpeed = -400;
-                hinge.useMotor = true;
-
-            }
-            else
-            {
-                motor.motorSpeed = 0;
-                hinge.useMotor = false;
-
-            }
-            hinge.motor = motor;
-        }
-
     }
 }
