36e4d01d4f421490fdabd12535159fc6a8341fc2
Evan McConnell <mcconnell.evan@gmail.com>
2020-02-01 03:42:40 +0000

    l

---
+++ Assets/_Scripts/Grab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    void OnCollisionStay(Collision stay)
+    {
+        print("we're in");
+        if (stay.gameObject.tag == "Grabbable")
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                GetComponent<HingeJoint2D>().connectedBody = stay.gameObject.GetComponent<Rigidbody2D>();
+                transform.parent.gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                print("grab!");
+            }
+        }
+    }
+}
