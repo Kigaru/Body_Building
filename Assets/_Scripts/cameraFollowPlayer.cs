8c22920f7be866356cb945247b67e83529d502d1
Evan McConnell <mcconnell.evan@gmail.com>
2020-02-01 00:33:17 +0000

    CameraFollowPlayer

---
+++ Assets/_Scripts/cameraFollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    //public int distanceFromPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -100);
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+}
