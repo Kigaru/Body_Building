d10d3756ea547e8580a67c725db2d5462f942a43
Kigaru <jakubster9@gmail.com>
2020-02-01 18:19:25 +0000

    Added Color distinction for player

--- Assets/_Scripts/CollideOnPoint.cs
+++ Assets/_Scripts/CollideOnPoint.cs
@@ -68,7 +68,7 @@ public class CollideOnPoint : MonoBehaviour
                 GetComponent<SpinJoint>().setButton(button);
                 GetComponent<SpinJoint>().setPlayerTwo(true);
                 textmeshPro.SetText(rightButtons[randomInt].ToString());
-
+                GetComponent<SpriteRenderer>().color = Color.red;
             }
             else
             {
@@ -76,6 +76,7 @@ public class CollideOnPoint : MonoBehaviour
                 GetComponent<SpinJoint>().setButton(button);
                 GetComponent<SpinJoint>().setPlayerTwo(false);
                 textmeshPro.SetText(leftButtons[randomInt].ToString());
+                GetComponent<SpriteRenderer>().color = Color.blue;
 
 
             }
---
+++ Assets/_Scripts/getOutOfMySwampOrIBiteYourArmOff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getOutOfMySwampOrIBiteYourArmOff : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] sprites;
+    [SerializeField]
+    GameObject[] arms;
+    [SerializeField]
+    float minX, maxX;
+
+    private IEnumerator coroutine;
+        
+    void Start()
+    {
+        coroutine = WaitAndPrint(5.0f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+            float pos = Random.Range(minX, maxX);
+            GameObject arm = Instantiate(arms[Random.Range(0, 1)],new Vector3(pos,10,0),Quaternion.identity);
+            arm.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length - 1)];
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+}
