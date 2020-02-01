854452ee70b65de59485d32be2710268955a8de6
Evan McConnell <mcconnell.evan@gmail.com>
2020-02-01 00:17:11 +0000

    TwoArmFunTimes

---
+++ Assets/EvanSpinJoint.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvanSpinJoint : MonoBehaviour
+{
+    HingeJoint2D hinge;
+    JointMotor2D motor;
+    KeyCode key;
+    public bool p2;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+        hinge.useMotor = true;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (p2 == true)
+        {
+            motor = hinge.motor;
+
+            if (Input.GetKey(KeyCode.K))
+            {
+                motor.motorSpeed = 400;
+            }
+            else if (Input.GetKey(KeyCode.L))
+            {
+                motor.motorSpeed = -400;
+            }
+            else
+            {
+                motor.motorSpeed = 0;
+            }
+            hinge.motor = motor;
+        }
+
+        else
+        {
+            motor = hinge.motor;
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                motor.motorSpeed = 400;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                motor.motorSpeed = -400;
+            }
+            else
+            {
+                motor.motorSpeed = 0;
+            }
+            hinge.motor = motor;
+        }
+
+    }
+}
--- Assets/_Scripts/ArmCollector.cs
+++ Assets/_Scripts/ArmCollector.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ArmCollector : MonoBehaviour
 {
@@ -19,13 +17,13 @@ public class ArmCollector : MonoBehaviour
         print("it's ya boy");
     }
 
-    void OnTriggerEnter(Collider coll)
+    void OnTriggerEnter(Collider hit)
     {
         print("I'm in");
-        if (coll.gameObject.tag == "Untagged")
+        if (hit.gameObject.tag == "Finish")
         {
             print("hit");
-            Destroy(coll.gameObject);
+            Destroy(hit.gameObject);
             nextArm = Instantiate(armPiece, spawnPoint.transform);
             nextArm.transform.parent = transform.parent;
             nextArm.GetComponent<HingeJoint2D>().connectedBody = GetComponent<Rigidbody2D>();
