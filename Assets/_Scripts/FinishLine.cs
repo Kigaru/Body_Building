58ed0618904e232576a6b9f9eed099599eaa4bc5
Kigaru <jakubster9@gmail.com>
2020-02-02 16:48:24 +0000

    Updated tiles & Linked End screen to libirtinth

---
+++ Assets/_Scripts/FinishLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField]
+    int exitNumber;
+    [SerializeField]
+    GameObject player1, player2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "head1")
+        {
+            backToMenu.head = collision.gameObject;
+            backToMenu.exitNumber = exitNumber;
+            backToMenu.scoreX = player1.GetComponent<updateNumberText>().LimbsCount;
+            backToMenu.scoreY = player2.GetComponent<updateNumberText>().LimbsCount;
+            SceneManager.LoadScene("End");
+        }
+    }
+
+}
