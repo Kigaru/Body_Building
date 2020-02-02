69b11816f0f40ebe91f9a50da29338239678ab9e
Kigaru <jakubster9@gmail.com>
2020-02-02 16:26:37 +0000

    Added End Scene

--- Assets/backToMenu.cs
+++ Assets/backToMenu.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class backToMenu : MonoBehaviour
 {
+    public static GameObject head;
+    public static int scoreX;
+    public static int scoreY;
+    public static int exitNumber;
     public string scene;
     public void LoadScene()
     {
+        exitNumber = 0;
+        scoreX = 0;
+        scoreY = 0;
+        head = null;
         SceneManager.LoadScene(scene);
     }
 }
