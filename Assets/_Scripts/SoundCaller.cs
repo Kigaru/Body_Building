9525b91d41db459db3ebf8c2377211b0d0001aea
Evan McConnell <mcconnell.evan@gmail.com>
2020-02-02 12:22:39 +0000

    Evan's Magical Soundy Sounds

---
+++ Assets/_Scripts/SoundCaller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCaller : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private int clipChoice;
+
+    public AudioClip[] slapSounds;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playArmSlap();
+        }
+
+    }
+
+    void playArmSlap()
+    {
+        clipChoice = Random.Range(0, slapSounds.Length-1);
+        audioSource.PlayOneShot(slapSounds[clipChoice]);
+    }
+}
