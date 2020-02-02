558f64a28ea94919658f6ae4177994f00c565857
Evan McConnell <mcconnell.evan@gmail.com>
2020-02-02 15:17:51 +0000

    Home Menu and Pause Menu

---
+++ Assets/_Scripts/Pause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+    bool ResumeButtonPressed = false;
+    public GameObject pMenu;
+
+    void Start()
+    {
+        //pMenu = GameObject.FindGameObjectWithTag("Pause Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paws();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+
+    void Paws()
+    {
+        pMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+}
