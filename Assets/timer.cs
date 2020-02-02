30b99d55d532b3d30f7a796e40cc3e7c46222b85
ciaranmalone <ciaranmalone27@gmail.com>
2020-02-02 12:41:56 +0000

    GIT

---
+++ Assets/timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class timer : MonoBehaviour
+{
+    TextMeshPro textmeshPro;
+    float time;
+
+    void Start()
+    {
+        textmeshPro = GetComponentInChildren<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        textmeshPro.SetText(time.ToString());
+    }
+}
