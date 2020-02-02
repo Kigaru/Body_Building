2f0809540215340b6822340d4b78eb71bf8d05ea
ciaranmalone <ciaranmalone27@gmail.com>
2020-02-02 14:07:50 +0000

    fixed bugs

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+   
+
+    public void LoadLimbRant()
+    {
+        SceneManager.LoadScene("Limberick");
+    }
+    public void LoadVerses()
+    {
+        SceneManager.LoadScene("VsRace");
+    }
+
+}
